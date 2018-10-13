0a5d2376fccdc9ca3b86ac4394acfd5f5ac6ea06
Carlo Fernando <ca.fernando415@gmail.com>
2018-10-13 04:26:59 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -29,7 +29,7 @@ end
 
 def shortened_tweet_truncator(tweet)
   shortened_tweet = word_substituter(tweet)
-  if shortened_tweet.length > 140
+  if shortened_tweet.length >= 140
     shortened_tweet = shortened_tweet[1...140]
   else
     shortened_tweet
